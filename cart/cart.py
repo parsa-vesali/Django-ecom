794f0ec033a157031d171c6c14debed4b9768cab
parsavesali <parsavesali84@gmial.com>
2025-10-11 16:41:55 +0330

    add Cart Feature

--- cart/cart.py
+++ cart/cart.py
@@ -1,25 +1,30 @@
+from store.models import Product
 class Cart():
-     def __init__(self,request):
-          self.session = request.session
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get('session_key')
 
-          cart = self.session.get('session_key')
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
 
-          if 'session_key' not in request.session:
-               cart = self.session['session_key'] = {}
+        self.cart = cart
 
-          self.cart = cart
+    def add(self, product):
+        product_id = str(product.id)
 
-     
-     def add(self,product):
-          product_id = str(product.id)
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
 
-          if product_id in self.cart:
-               pass
-          else :
-               self.cart[product_id] = {'price' : str(product.price)}
+        self.session.modified = True
 
-          self.session.modified= True
+    def __len__(self):
+        return len(self.cart)
+    
 
+    def get_prods(self):
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
 
-     def __len__(self):
-          return len(self.cart)
+        return products
