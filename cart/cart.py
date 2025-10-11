731dbbf4d1257018b938533cfee05a003237cbe2
parsavesali <parsavesali84@gmial.com>
2025-10-11 18:50:06 +0330

    Feat: add quantity product in cart

--- cart/cart.py
+++ cart/cart.py
@@ -9,13 +9,15 @@ class Cart():
 
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product,quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
 
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
 
         self.session.modified = True
 
@@ -28,3 +30,9 @@ class Cart():
         products = Product.objects.filter(id__in=product_ids)
 
         return products
+    
+
+
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
