cedbdac789b8582545e2181033d8a0dc0db66a18
parsavesali <parsavesali84@gmial.com>
2025-10-23 12:19:47 +0330

    feat(checkout) : add checkout page

--- payment/models.py
+++ payment/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from store.models import Product
+from django.db.models.signals import post_save
 
 class ShippingAddress(models.Model):
      user = models.ForeignKey(User,on_delete=models.CASCADE,null=True, blank=True)
@@ -19,3 +20,44 @@ class ShippingAddress(models.Model):
 
      def __str__(self):
           return f'Shipping address - {str(self.id)}'
+     
+
+# Create user profile by deafult
+def create_shipping(sender ,instance , created,**kwargs):
+    if created:
+        user_shipping = ShippingAddress(user=instance)
+        user_shipping.save()
+
+post_save.connect(create_shipping,sender=User)
+
+class Order(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True, blank=True)
+    full_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=20 , blank=True)     
+    shipping_address = models.CharField(max_length=200 , blank=True)
+    amount_paid = models.DecimalField(
+        max_digits=12,  
+        decimal_places=0,
+        default= 0 ,
+    )
+    date_ordered = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+         return f"orser {str(self.id)}"
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order , on_delete=models.CASCADE , null=True)
+    product = models.ForeignKey(Product , on_delete=models.CASCADE , null=True)
+    user = models.ForeignKey(User , on_delete=models.CASCADE , null=True)
+
+    quantity = models.PositiveBigIntegerField(default=1)
+    price = models.DecimalField(
+        max_digits=12,  
+        decimal_places=0,
+        default= 0 ,
+    )
+
+
+    def __str__(self):
+         return f'order item {str(self.id)}'
--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from cart.cart import Cart
+
+from payment.forms import shippingForm , ShippingAddress
+
+def checkout(request):
+     # Get cart
+     cart = Cart(request)
+     cart_products = cart.get_prods()
+     quantities = cart.get_quants
+     totals = cart.cart_total()
+     shipping_user, created = ShippingAddress.objects.get_or_create(user=request.user)
+
+     if request.user.is_authenticated:
+          shipping_form = shippingForm(request.POST or None, instance=shipping_user)
+          return render(request , "payment/checkout.html" , {"cart_products" : cart_products , "quantities" : quantities ,"totals" : totals , 'shipping_form' : shipping_form})
+     else:
+          shipping_form = shippingForm(request.POST or None)
+          return render(request , "payment/checkout.html" , {"cart_products" : cart_products , "quantities" : quantities ,"totals" : totals, 'shipping_form' : shipping_form})
+
 
 def payment_success(request):
      return render(request,"payment/payment_success.html" , {})
